be506f490a51b07d38b559d43cbe4c64ea8e7571
Utitofon-Udoekong <mosesrahul37@gmail.com>
2022-05-17 22:20:09 +0100

    ok

--- server/api/users.ts
+++ server/api/users.ts
@@ -1,26 +1,27 @@
 import { IncomingMessage, ServerResponse } from "http";
 import { db } from "~~/helpers/fireadmin";
 
-// import { getFirestore } from 'firebase-admin/firestore'
-// import { initializeApp, getApps, cert } from 'firebase-admin/app'
-
-// const apps = getApps()
-// if (!apps.length) {
-//     initializeApp({
-//         credential: cert('./serviceAccount.json') // 👈 Path to your JSON Firebase certificate
-//     })
-// }
-
 export default async (request:IncomingMessage, response:ServerResponse) => {
     // const db = getFirestore()
-    const usersSnap = await db.collection('authUsers').get();
-    if(usersSnap.empty) return "No user created"
-    const users = usersSnap.docs.map((doc) => {
-        return {
-            uuid: doc.id,
-            ...doc.data(),
-        }
-    })
+    // const usersSnap = await db.collection('authUsers').get();
+    // if(usersSnap.empty) return "No user created"
+    // const users = usersSnap.docs.map((doc) => {
+    //     return {
+            // uuid: doc.id,
+            // ...doc.data(),
+    //     }
+    // })
     
+    // return users
+    let users = []
+    db.collection('authUsers').onSnapshot((querySnapshot) => {
+        querySnapshot.forEach((doc) => {
+            users.push({
+                uuid: doc.id,
+                ...doc.data(),
+            });
+        });
+
+    })
     return users
 }
