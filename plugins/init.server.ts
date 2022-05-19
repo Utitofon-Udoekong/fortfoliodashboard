cb95c42b5f92dc5c51704d75d5f148f1a24f2307
Utitofon-Udoekong <mosesrahul37@gmail.com>
2022-05-20 00:27:40 +0100

    ok

--- plugins/init.server.ts
+++ plugins/init.server.ts
@@ -3,14 +3,8 @@ import { useUserStore } from '~~/store/users'
 
 export default defineNuxtPlugin(async (nuxtApp) => {
   const users = useUserStore(nuxtApp.$pinia)
-  console.log("initing user and kyc")
+  console.log("initing user")
   // await users.setUsers()
-  await Promise.all([users.setInvestments()])
-  // await Promise.all([
-  //   await users.setUsers(),
-  //   await users.setKyc(),
-  //   await users.setInvestments(),
-  //   await users.setWithdrawals(),
-  // ])
-  console.log("inited user and kyc")
+  await users.setUsers()
+  console.log("inited user")
 })
