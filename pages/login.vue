f38b11cd5a25a34b3c9c652ba9ef71a2ed81103b
Utitofon-Udoekong <mosesrahul37@gmail.com>
2022-05-24 10:56:02 +0100

    ok

--- pages/login.vue
+++ pages/login.vue
@@ -1,23 +1,22 @@
 <script setup lang="ts">
 import { useUserStore } from "~~/store/users";
-const store = useUserStore()
-const router = useRouter()
-const {$auth} = useNuxtApp()
+const store = useUserStore();
+const router = useRouter();
+const { $auth } = useNuxtApp();
 
 interface Login {
-    email: string,
-    password: string,
-    confirmed: string
+  email: string;
+  password: string;
+  confirmed: string;
 }
-let email = ref("")
-let password = ref("")
+let email = ref("");
+let password = ref("");
 
 const login = async () => {
   await store.login().then(() => {
-    router.push("/dashboard")
-  })
-}
-
+    router.push("/dashboard");
+  });
+};
 </script>
 
 <template>
@@ -25,30 +24,43 @@ const login = async () => {
     <Html>
       <Head>
         <Title>Login - Admin</Title>
-        <Meta name="description" content="Fortfolio Admin Signup page"/>
+        <Meta name="description" content="Fortfolio Admin Signup page" />
       </Head>
     </Html>
     <div class="flex justify-center items-center h-full">
-      <div
-        class="max-w-sm flex flex-col"
-      >
-        <div class="header w-full p-8 bg-brand-blue">
-          <h2 class="title p-2 text-center mb-6 text-white ">
-          Login Admin
-        </h2>
+      <div class="w-80 flex flex-col border">
+        <div class="header w-full bg-brand-blue mb-3 grid place-items-center">
+          <h2 class="text-xl p-2 mb-6 text-white">Login Admin</h2>
         </div>
-       <div class="body p-8">
-          <label for="email">Email</label>
-        <input type="email" class="p-2" id="email" required v-model="email">
-        <label for="password">Password</label>
-        <input type="text" id="password" class="p-2" required v-model="password">
-        <button @click="login"
-              class="hover:underline-current text-brand-light-blue text-sm"
-              >Signup</button
-            >
-       </div>
+        <div class="p-10">
+          <div class="formgroup mb-6 flex flex-col">
+            <label for="email">Email</label>
+            <input
+              type="email"
+              class="p-2"
+              id="email"
+              required
+              v-model="email"
+            />
+          </div>
+          <div class="formgroup mb-6 flex flex-col">
+            <label for="password">Password</label>
+            <input
+              type="text"
+              id="password"
+              class="p-2"
+              required
+              v-model="password"
+            />
+          </div>
+          <button
+            @click="login"
+            class="hover:underline-current text-white p-4 text-center text-md w-full bg-brand-blue"
+          >
+            Signup
+          </button>
+        </div>
       </div>
     </div>
   </div>
 </template>
-
