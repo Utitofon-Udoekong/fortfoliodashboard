c099e9c3ddaabfdead6c0200850c3cc5f0bae55f
Utitofon-Udoekong <mosesrahul37@gmail.com>
2022-05-27 01:15:27 +0100

    ok

--- composables/useFirebase.ts
+++ composables/useFirebase.ts
@@ -69,9 +69,7 @@ export const initUser = async () => {
     if (user) {
       // User is signed in, see docs for a list of available properties
       // https://firebase.google.com/docs/reference/js/firebase.User
-      await store.login().then(() => {
-        router.push("/dashboard/investment")
-      })
+      await store.login()
     } else {
       router.replace("/login")
     }
