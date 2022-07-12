33925ba1bbdb98b9fdf40c80f89cb07137ce7249
Utitofon-Udoekong <mosesrahul37@gmail.com>
2022-07-12 13:30:10 +0100

    ok

--- server/api/disable.post.ts
+++ server/api/disable.post.ts
@@ -1,23 +1,19 @@
-// import { getAuth } from "firebase-admin/auth";
-// import { db } from "~~/helpers/fireadmin";
+import { getAuth } from "firebase-admin/auth";
+import { db } from "~~/helpers/fireadmin";
 
-// export default async (req) => {
-//     const {uid} = await useBody(req)
-//     console.log(uid)
-//     await getAuth()
-//             .updateUser(uid, {
-//                 disabled: true,
-//             })
-//             .then((_) => {
-//                 db.collection("authUsers").doc(uid).update({
-//                     status: "Disabled"
-//                 })
-//                 // return 'Successfully disabled user'
-//             })
-//             .catch((error) => {
-//                 return "Error disabling user " + error
-//             })}
 export default defineEventHandler(async (event) => {
     const uid = await useBody(event)
-    return uid
+    await getAuth()
+            .updateUser(uid, {
+                disabled: true,
+            })
+            .then((_) => {
+                db.collection("authUsers").doc(uid).update({
+                    status: "Disabled"
+                })
+                // return 'Successfully disabled user'
+            })
+            .catch((error) => {
+                return "Error disabling user " + error
+            })
 })
