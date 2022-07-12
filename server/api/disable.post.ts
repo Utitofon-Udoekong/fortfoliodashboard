f31051ee4e0de53dbb0e8d2e1a62fac7991fe566
Utitofon-Udoekong <mosesrahul37@gmail.com>
2022-07-12 18:18:52 +0100

    ok

--- server/api/disable.post.ts
+++ server/api/disable.post.ts
@@ -2,17 +2,14 @@ import { auth, db } from "~~/helpers/fireadmin";
 
 export default defineEventHandler(async (event) => {
     const uid = await useBody(event)
-    await auth
-            .updateUser(uid, {
-                disabled: true,
-            })
-            .then((_) => {
-                db.collection("authUsers").doc(uid).update({
-                    status: "Disabled"
-                })
-                return 'Successfully disabled user'
-            })
-            .catch((error) => {
-                return "Error disabling user " + error
-            })
+    await db.collection("authUsers").doc(uid).update({
+        status: "Disabled"
+    }).then(async () => {
+        await auth.updateUser(uid, {
+            disabled: true,
+        })
+        return 'Successfully disabled user'
+    }).catch((error) => {
+            return "Error disabling user " + error
+        })
 })
