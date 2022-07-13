500842a2fb8144f75c89e19d8a5c28c677f653d9
Utitofon-Udoekong <mosesrahul37@gmail.com>
2022-07-13 20:35:38 +0100

    ok

--- store/userStore.ts
+++ store/userStore.ts
@@ -1,39 +1,76 @@
 import { defineStore } from "pinia";
 
-export const useUserStore = defineStore('user', {
-    // state: () => ({
-    //     loading: false,
-    //     notificationMessage: "",
-    //     showSuccess: false,
-    //     showFailure: false,
-    // }),
-    // actions: {  
-    //     async disableUser(uid: string){
-    //         const {data} = await useAsyncData('disable', () => $fetch('/api/disable', {
-    //             method: "POST",
-    //             body: uid
-    //         }))
-    //         const message = data.value
-    //         console.log(message)
-    //         this.notificationMessage = message
-    //     },
-    //     async enableUser(uid: string){
-    //         const {data} = await useAsyncData('enable', () => $fetch('/api/enable', {
-    //             method: "POST",
-    //             body: uid
-    //         }))
-    //         const message = data.value
-    //         console.log(message)
-    //         return message
-    //     },
-    //     async deleteUser(uid: string){
-    //         const {data} = await useAsyncData('delete', () => $fetch('/api/delete', {
-    //             method: "POST",
-    //             body: uid
-    //         }))
-    //         const message = data.value
-    //         console.log(message)
-    //         return message
-    //     }
-    // }
+export const useUserStore = defineStore('user', () => {
+    const loading = ref(false)
+    const notificationMessage = ref("")
+    const showSuccess = ref(false)
+    const showFailure = ref(false)
+    const setLoading = (val) => loading.value = val
+
+    const enableUser = async (uid: string) => {
+        loading.value = true
+        const {data} = await useAsyncData('enable', () => $fetch('/api/enable', {
+            method: "POST",
+            body: uid
+        }))
+        const message: string = data.value
+        console.log(message)
+        notificationMessage.value = message
+        loading.value = false
+        if(message.includes("Success")) showSuccess.value = true
+        else showFailure.value = true
+    }
+    const disableUser = async (uid: string) => {
+        loading.value = true
+        const {data} = await useAsyncData('disable', () => $fetch('/api/disable', {
+            method: "POST",
+            body: uid
+        }))
+        const message: string = data.value
+        console.log(message)
+        notificationMessage.value = message
+        loading.value = false
+        if(message.includes("Success")) showSuccess.value = true
+        else showFailure.value = true
+    }
+    const deleteUser = async (uid: string) => {
+        loading.value = true
+        const {data} = await useAsyncData('delete', () => $fetch('/api/delete', {
+            method: "POST",
+            body: uid
+        }))
+        const message: string = data.value
+        console.log(message)
+        notificationMessage.value = message
+        loading.value = false
+        if(message.includes("Success")) showSuccess.value = true
+        else showFailure.value = true
+    }
+
+    watch(showFailure, (newVal) => {
+        if (newVal === true) {
+          setTimeout(() => {
+            showFailure.value = false;
+          }, 1500);
+        }
+      });
+      
+      watch(showSuccess, (newVal) => {
+        if (newVal === true) {
+          setTimeout(() => {
+            showSuccess.value = false;
+          }, 1500);
+        }
+      });
+
+      return {
+        loading,
+        notificationMessage,
+        showSuccess,
+        showFailure,
+        setLoading,
+        enableUser,
+        disableUser,
+        deleteUser
+      }
 })
