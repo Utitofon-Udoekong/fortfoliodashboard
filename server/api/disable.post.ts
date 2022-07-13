339c9d13373074d753450c02ce401710a9d2c1d9
Utitofon-Udoekong <mosesrahul37@gmail.com>
2022-07-13 13:31:28 +0100

    ok

--- server/api/disable.post.ts
+++ server/api/disable.post.ts
@@ -2,16 +2,20 @@ import { auth, db } from "~~/helpers/fireadmin";
 
 export default defineEventHandler(async (event) => {
     const uid = await useBody(event)
-    await auth.updateUser(uid, {
-                disabled: true,
-            })
-            .then(async (_) => {
-                await db.collection("authUsers").doc(uid).update({
-                    status: "Disabled"
-                })
-                return 'Successfully disabled user'
-            })
-            .catch((error) => {
-                return "Error disabling user " + error
-            })
+    await db.collection("authUsers").doc(uid).update({
+        status: "Disabled"
+    })
+    return 'Successfully disabled user'
+    // await auth.updateUser(uid, {
+    //             disabled: true,
+    //         })
+    //         .then(async (_) => {
+                // await db.collection("authUsers").doc(uid).update({
+                //     status: "Disabled"
+                // })
+                // return 'Successfully disabled user'
+    //         })
+    //         .catch((error) => {
+    //             return "Error disabling user " + error
+    //         })
 })
--- store/userStore.ts
+++ store/userStore.ts
@@ -3,6 +3,7 @@ import { defineStore } from "pinia";
 export const useUserStore = defineStore('user', {
     actions: {        
         async disableUser(uid: string){
+            console.log(uid)
             const {data} = await useAsyncData('disable', () => $fetch('/api/disable', {
                 method: "POST",
                 body: {uid}
